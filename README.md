3db2d60259879c4a6986e68301a90874f25d4a35
czaky <jendrzej.w@lczak.net>
2022-12-10 23:22:35 +0000

    Initial commit including: glados, tiff, and sweetie-belle

---
+++ README.md
@@ -0,0 +1,83 @@
+# Voice Packs for Dreame Vacuum Robots
+
+Uses voice generation by [uberduck](https://uberduck.ai/).
+
+Works at least with `L10s Ultra`, `L10 Pro`, `Z10 Pro` and `W10`.
+Optimized for [valetudo](https://valetudo.cloud/) use.
+
+## Installation
+
+1. In Valetudo go to "Robot Settings" -> "Misc Settings"
+2. Enter the following information in the "Voice packs" section:
+    - URL: https://github.com/\<repo\>/\<character\>/voice.tar.gz
+    - Language Code: `CUSTOM` - or anything but language codes like `EN`
+    - Hash: Lookup in the \<character\>/`HASH.txt` file
+3. Click "Set Voice Pack"
+
+4. Check if `.ogg` files have been copied to `/data/personalized_voice/CUSTOM` directory on the robot.
+5. If not, extract the tar.gz and copy the .ogg files using your favorite `scp` tool. 
+
+## Generation
+
+### Prerequisites
+
+1. Install `ffmpeg` and `vorbis-tools`
+
+```sh
+sudo apt install ffmpeg vorbis-tools
+```
+
+2. Install uberduck python API and ffmpeg-normalize
+
+```sh
+pip install uberduck ffmpeg-normalize
+```
+
+### Invocation
+
+1. Sing up with https://uberduck.ai
+2. Generate a public key and a secret token in your profile.
+3. Replace the public keys and tokens in the `keys.csv`
+4. Run the script (replacing `kirby-tiff` with one of the voices)
+
+```sh
+python generate.py --voice kirby-tiff [--volume 1.1] [--normalize]
+```
+
+The default voice is `glados`.
+
+Note: Normalization uses `ffmpeg` and may create random artifacts in the resulting audio. It is not recommended with Uberduck.AI voice characters.  
+
+Note: voice list can be found at https://app.uberduck.ai/quack-help . 
+Not all voices are good. Please, consult: https://app.uberduck.ai/leaderboard/voice
+
+Listen to the resulting `.ogg` files for any artifacts. Change the text if necessary.
+
+### Modify
+
+The defaults are loaded first and are found in the `defaults` folder.
+Then the values in each character folder override the defaults.
+Each folder can have following files:
+
+ * `./sayings.csv` - a list of saying IDs and saying texts. If the text is empty, generator will look for a prepared `.ogg` file.
+ * `./replacement.csv` - a list of words and their replacements, in case the AI characters cannot pronounce those correctly.
+ * `./*.ogg` - prepared ogg audio files, used when the text for the saying ID is empty.
+
+### Results
+
+In each voice-character folder:
+
+ * `./voice.tar.gz` - archive of the voices generated.
+ * `./HASH.txt` - contains the md5 hash code of the voice archive.
+ * `./ogg/*.ogg` - resulting ogg audio files, that go into the archive.
+ * `./tts/*.wav` - transformed text audios with name being the hash of the text.
+
+## Contribute
+
+Please, don't add any obscenity or racist stuff. We cannot accept this on a public directory. There is no point in offending anybody. Reserve this stuff for your local installation.
+
+Also, please, don't add anything copy-protected.
+
+## Thanks
+
+Thanks to https://github.com/ccoors/dreame_voice_packs and https://github.com/Findus23/voice_pack_dreame for the inspiration and https://uberduck.ai for the voice generation.
---
+++ generate.py
@@ -0,0 +1,274 @@
+# Script to generate Dreame robot voice-packs using uberduck.ai TTS.
+
+import errno
+import os
+import shutil
+import signal
+import argparse
+from subprocess import call, run
+
+import re
+import csv
+import hashlib
+from pathlib import Path
+from typing import Dict
+
+from uberduck import UberDuck
+
+# Return a hash string for the THING 
+def hash(thing) -> str:
+    return hashlib.md5(str(thing).encode('utf-8')).hexdigest()
+
+# Used to timeout TTS API calls
+class TimeoutError(Exception):
+    pass
+
+# TTS API initialization error.
+class MissingTextToSpeechServerError(Exception):
+    pass
+
+# Interacts the the Uberduck.AI API.
+class TTS():
+    DEFAULT_TIMEOUT = 20
+
+    def __init__(self, pub, pk, voice, timeout = None):
+        self.pub = pub
+        self.pk = pk
+        self.voice = voice
+        self.timeout = timeout if timeout else TTS.DEFAULT_TIMEOUT
+        self.api = None
+
+    def connect(self):
+        try:
+            self.api = UberDuck(self.pub, self.pk)
+            if self.api: return True
+        except Exception as e:
+            print(f"Could not connect to UberDuck using {self.pub} and {self.pk}.")
+        return False
+
+    def transform(self, text, output, timeout):
+        if len(text) == 0: 
+            print(f"Text for {output} is empty.")
+            return False
+        try:   
+            tmp = f"{output}.tmp"     
+            if os.path.exists(tmp): os.remove(tmp)
+            result = self._get_audio(text, file_path = tmp, timeout = timeout)
+            if result:
+                os.replace(tmp, output)
+                return True
+        except Exception as e:
+            print(f"Exception while transforming {output}: {e}.")
+            self.api = None
+
+        return False
+
+    def _on_timeout(self, signum, frame):
+        raise TimeoutError('Uberduck.AI timed out.')
+
+    def _get_audio(self, text, file_path, timeout = None):
+        if self.api is None and not self.connect(): return False
+
+        signal.signal(signal.SIGALRM, self._on_timeout)
+        try:
+            signal.alarm(timeout if timeout else self.timeout)
+            result = self.api.speak(text, self.voice, file_path = file_path)
+        finally:
+            signal.alarm(0)
+        return result
+        
+# Reads the configs, sayings, and transforms the sayings' text to a voice pack.
+class Generator():
+
+    DEFAULT_VOICE = 'glados' 
+    DEFAULT_DIR = Path("default/")
+
+    # Initializes the generator for the given uberduck.ai voice target.
+    #
+    # volume - is passed to ffmpeg to change the volume of the resulting TTS speech
+    # normalize - flag that enables calling ffmpeg-normalize on the TTS output
+    # timeout - the default timeout used for the TTS API
+    def __init__(self, voice = None, 
+                volume = None, normalize = False,
+                timeout = None):
+        if voice is None: voice = Generator.DEFAULT_VOICE
+        if timeout is None: timeout = TTS.DEFAULT_TIMEOUT
+        self.voice = str(voice).strip().lower()
+        self.dir = Path(f"{voice}/")
+        self.volume = volume
+        self.normalize = normalize
+        self.timeout = timeout
+
+        keys = self.load_keys("keys.csv")
+        #print(keys)
+        all_tts = [TTS(pub, pk, voice = self.voice, timeout = self.timeout) for pub,pk in keys]
+        self.tts = [tts for tts in all_tts if tts.connect()]
+
+        if len(self.tts) == 0:
+            raise MissingTextToSpeechServerError("No usable TTS keys found. Aborting voice generation.")
+            
+        default_replacements = self.load_replacements(Generator.DEFAULT_DIR / "replacement.csv")
+        raw_replacements = self.load_replacements(self.dir / "replacement.csv", default_replacements)
+        # print(raw_replacements)
+        self.replacements = self.prepare_replacements(raw_replacements)
+        # print(self.replacements)
+
+        default_sayings = self.load_sayings(Generator.DEFAULT_DIR / "sayings.csv")
+        self.sayings = self.load_sayings(self.dir / "sayings.csv", default_sayings)
+
+    # Loads the TTS API keys from the provided file.
+    def load_keys(self, filepath) -> list:
+        data = []
+        line = 0
+        with open(filepath) as file:
+            for row in csv.reader(file, skipinitialspace=True):
+                line += 1
+                if len(row) == 0: continue
+                if len(row) == 1 and len(str(row[0]).strip()) == 0: continue
+                if len(row) != 2 or len(str(row[0]).strip()) == 0 or len(str(row[1]).strip()) == 0:
+                    row_str = "','".join(row)
+                    print(f"ERROR: Need two keys (pub, pk) in line [{filepath}:{line}] '{row_str}'")
+                    continue
+                data.append(row)
+        return data
+
+    # Loads text replacements from the given file. 
+    def load_replacements(self, filepath, data = {}) -> Dict[str, str]:
+        if not os.path.exists(filepath): return data 
+        line = 0
+        with open(filepath) as file:
+            for row in csv.reader(file, skipinitialspace=True):
+                line += 1
+                if len(row) == 0: continue
+                if len(row) == 1 and len(str(row[0]).strip()) == 0: continue
+                if len(row) != 2 or len(str(row[0]).strip()) == 0 or len(str(row[1]).strip()) == 0:
+                    row_str = "','".join(row)
+                    print(f"ERROR: Need before and after strings for replacement [{filepath}:{line}] '{row_str}'")
+                    continue
+                data[str(row[0]).strip().lower()] = str(row[1]).strip().lower()
+        return data
+
+    # Compiles the replacements as regular expressions.
+    def prepare_replacements(self, replacements):
+        result = []
+        for search, replace in replacements.items():
+            result.append((re.compile(f"\\b{search}\\b"), replace))
+        return result
+        
+    # Applies the replacements to the text in a serial manner.
+    def apply_replacements(self, text):
+        for search, replace in self.replacements:
+            text = re.sub(search, replace, text)
+        return text
+
+    # Loads and normalizes the text for each robot saying. Returns a dictionary.
+    def load_sayings(self, filepath, data = {}) -> Dict[int, str]:
+        if not os.path.exists(filepath): return data 
+        line = 0
+        with open(filepath) as file:
+            for row in csv.reader(file, skipinitialspace=True):
+                line += 1
+                if len(row) == 0: continue
+                if len(row) == 1 and len(str(row[0]).strip()) == 0: continue
+                if len(row) != 2 or len(str(row[0]).strip()) == 0:
+                    row_str = "','".join(row)
+                    print(f"ERROR: Need a number and a text for the saying: [{filepath}:{line}] '{row_str}'")
+                    continue
+                try:
+                    id = int(row[0])
+                    text = self.apply_replacements(str(row[1]).strip().lower())
+                    data[id] = text
+                except Exception as e:
+                    row_str = "','".join(row)
+                    print(f"Error in line [{filepath}:{line}] '{row_str}' ...\n {e}")
+                    continue
+        return data
+
+    # Applies some transformations on the TTS audio like ffmpeg-normalize
+    def process_audio(self, input, output):
+        audio = input
+        if self.volume:
+            tmp = f"{output}.tmp.{Path(output).suffix}"
+            if os.path.exists(tmp): os.remove(tmp)
+            run(["ffmpeg", "-hide_banner", "-loglevel", "error", 
+                "-i", str(audio), "-filter:a", f"volume={self.volume}", str(tmp)])
+            os.replace(tmp, output)
+            audio = output
+        if self.normalize:
+            run(["ffmpeg-normalize", "-q",  "-o", str(output), "-f", str(audio)])
+            audio = output
+        return audio
+
+    # Converts the audio to the vorbis OGG format.
+    def convert_to_ogg(self, input, output):
+        run(["oggenc", str(input), "--output", str(output), "--bitrate", "100", "--resample", "16000", "-Q"])
+
+    # Iterates through all the sayings, uses TTS and then normalizes audio if necessary.
+    def process(self):
+        default_dir = Path("default/")
+        voice_dir = Path(f"{self.voice}/")
+        if not os.path.exists(voice_dir): os.mkdir(voice_dir)
+        tts_dir = voice_dir / "tts/"
+        if not os.path.exists(tts_dir): os.mkdir(tts_dir)
+        ogg_dir = voice_dir / "ogg/"
+        if not os.path.exists(ogg_dir): os.mkdir(ogg_dir)
+
+        items = [] 
+        for id, text in self.sayings.items():
+            items.append((id, text, self.timeout))
+
+        files = []
+        count = 0
+        while len(items) > 0:
+            count += 1
+            id, text, timeout = items.pop(0)
+            ogg_path = ogg_dir / f"{id}.ogg"
+            print(f"Processing {ogg_path}: \"{text}\"")
+
+            if len(text) == 0:
+                default = voice_dir / f"{id}.ogg"
+                if not os.path.exists(default):     
+                    default = default_dir / f"{id}.ogg"
+                if os.path.exists(default):
+                    print(f"Reusing: {default}")
+                    shutil.copy(default, ogg_path)
+                    files.append(ogg_path.name)
+                    continue
+
+                print(f"WARNING: No default found for {ogg_path}: \"<empty-text>\"")
+                continue
+
+            md5 = hash(text)
+            tts_path = tts_dir / f"{md5}.wav"
+            
+            if not os.path.exists(tts_path):
+                # Transform text to speech
+                i = count % len(self.tts)
+                if not self.tts[i].transform(text, tts_path, timeout):
+                    items.append((id, text, round((2 + timeout) * 1.5) ))
+                    continue
+            
+            tmp_path = tts_dir / f"{md5}-processed.wav"
+            processed = self.process_audio(tts_path, tmp_path)
+            self.convert_to_ogg(processed, ogg_path)
+            files.append(ogg_path.name)
+
+        run(["tar", "-c", "-f", "../voice.tar.gz", *files], cwd = ogg_dir)
+        with open(voice_dir / "HASH.txt", "w") as file:
+            run(["md5sum", "--tag", "-b", "voice.tar.gz"], stdout= file, cwd = voice_dir)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Generate Dreame Voice Pack')
+    parser.add_argument('--voice', default = Generator.DEFAULT_VOICE)
+    parser.add_argument('--volume', default = None, 
+                        help='Volume parameter passed to ffmpeg as factor or db.')
+    parser.add_argument('--timeout', default = TTS.DEFAULT_TIMEOUT, type=int, 
+                        help='Timeout to be used with the TTS API.')
+    parser.add_argument('--normalize', action='store_true', 
+                        help='Instructs ffmpeg to normalize the sound.')
+    args = parser.parse_args()
+
+    # print(args)
+    g = Generator(args.voice, volume = args.volume, normalize = args.normalize, timeout = args.timeout)
+
+    g.process()
